a14abeeaac40b65b36497009cfa0313187c925bb
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-06-07 14:32:05 -0500

    new structure

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,18 +1,20 @@
 import 'dart:io';
 
+import 'package:dart_basics/my_converter.dart';
+
 void main(List<String> arguments) {
-  // TODO: Instancia [MainConverter]
+  final converter = MyConverter();
   print("Convertir:");
   print("1. Binario a Decimal");
   print("2. Decimal a Binario");
-  // TODO: Lee la entrada del usuario y conviertela en entero
-  if (/* TODO: Escribe la condicion adecuada para ingresar cuando se escoga 'Binario a Decimal' */) {
+  int option = int.parse(stdin.readLineSync()!);
+  if (option == 1) {
     print("Escribe el numero binario:");
-    // TODO: Lee la entrada del usuario y usa el setter correcto
-    print('Decimal: ${/* TODO: Ejecuta la conversion */}');
-  } else if (/* TODO: Escribe la condicion adecuada para ingresar cuando se escoga 'Decimal a Binario' */) {
+    converter.binary = stdin.readLineSync();
+    print('Decimal: ${converter.convertBinary()}');
+  } else if (option == 2) {
     print("Escribe el numero decimal:");
-    // TODO: Lee la entrada del usuario y usa el setter correcto
-    print('Binario: ${/* TODO: Ejecuta la conversion */}');
+    converter.decimal = stdin.readLineSync();
+    print('Binario: ${converter.convertDecimal()}');
   }
 }
